3ab1fa68f1c78ed2795717a1890ba14be5ccb80e
Valery_Ber <valera.beretar@yandex.ru>
2024-06-12 03:51:11 +0300

    v1: Прописал модели

--- translator_bot/bot/models.py
+++ translator_bot/bot/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Chat(models.Model):
+    """Модель чата"""
+
+    business = 'BU'
+    inventions = 'IN'
+    fashion = 'FA'
+
+    CATEGORY = [
+        (business, 'Бизнес'),
+        (inventions, 'Изобретения'),
+        (fashion, 'Мода')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message_user = models.TextField()
+    message_ai = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    statement = models.CharField(max_length=2, choices=CATEGORY, unique=True)
+
+    def __str__(self):
+        return f'{self.user.username} : {self.message_user[0:20]}'
