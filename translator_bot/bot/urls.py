94762474d6e981c67b992e717a791a11b8409310
Valery_Ber <valera.beretar@yandex.ru>
2024-06-24 00:18:23 +0300

    v1: Переделал url, теперь необходимо явно указывать чат в котором происходит диалог

--- translator_bot/bot/urls.py
+++ translator_bot/bot/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import ChatListView
 
 urlpatterns = [
-    path('chat/', ChatListView.as_view({'get': 'list', 'post': 'create'})),
+    path('chat/<str:statement>/', ChatListView.as_view({'get': 'list', 'post': 'create'})),
     path('chat/<int:pk>/', ChatListView.as_view({'delete': 'destroy'})),
 ]
--- translator_bot/bot/views.py
+++ translator_bot/bot/views.py
@@ -11,21 +11,14 @@ from .serializers import ChatSerializer
 class ChatListView(viewsets.ModelViewSet):
     serializer_class = ChatSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_fields = ['statement']
     ordering_fields = ['date_created']
     ordering = ['-date_created']
     permission_classes = (IsAuthenticated, )
 
     def get_queryset(self):
         user = self.request.user
-        statement = self.request.query_params.get('statement')
-        if statement:
-            return Chat.objects.filter(user=user)
-        else:
-            response_data = {
-                'messsage': 'Необходимо выбрать чат!'
-            }
-            return Response(response_data, status=status.HTTP_403_FORBIDDEN)
+        statement = self.kwargs.get('statement').upper()
+        return Chat.objects.filter(user=user, statement=statement)
 
 
 
